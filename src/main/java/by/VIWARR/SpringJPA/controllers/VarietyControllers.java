9e78f54aa6df9b2168b9f35bd32b3ee6ed4acc65
VIWARR <dm.pavlyuchenko@gmail.com>
2024-03-26 16:08:55 +0300

    start Variety

--- src/main/java/by/VIWARR/SpringJPA/controllers/VarietyControllers.java
+++ src/main/java/by/VIWARR/SpringJPA/controllers/VarietyControllers.java
@@ -1,16 +1,17 @@
 package by.VIWARR.SpringJPA.controllers;
 
+import by.VIWARR.SpringJPA.models.Product;
+import by.VIWARR.SpringJPA.models.Variety;
 import by.VIWARR.SpringJPA.services.ProductService;
 import by.VIWARR.SpringJPA.services.VarietyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/variety")
@@ -34,6 +35,7 @@ public class VarietyControllers {
         return "variety/variety_list";
     }
 
+    /*--------------NewVariety---------*/
     @GetMapping("/new")
     public String newVarietyForm(Model model) {
         model.addAttribute("products", productService.findAll());
@@ -41,11 +43,49 @@ public class VarietyControllers {
         return "variety/new_variety_form";
     }
 
+    /*-----------Create variety---------*/
     @PostMapping()
     public String create(HttpServletRequest request) {
         int id = Integer.parseInt(request.getParameter("product"));
         String name = request.getParameter("nameVariety");
         varietyService.save(name, id);
+
+        return "redirect:/variety";
+    }
+
+    /*------------отображение сорта по Id------------*/
+    @GetMapping("/id{id}")
+    public String showVarietyById(Model model, @PathVariable("id") int id) {
+        model.addAttribute("variety", varietyService.findOne(id));
+
+        return "variety/show_variety_by_id";
+    }
+
+    /*----------форма редактирования----------*/
+    @GetMapping("/id{id}/edit")
+    public String editForm(Model model, @PathVariable("id") int id) {
+        Variety variety = varietyService.findOne(id);
+        List<Product> products = productService.findAll();
+        products.remove(variety.getProduct());
+
+        model.addAttribute("variety", variety);
+        model.addAttribute("products", products);
+
+        return "variety/edit_form";
+    }
+
+    @PatchMapping("/id{id}")
+    public String editVariety(HttpServletRequest request, @PathVariable("id") int id) {
+        String newNameVariety = request.getParameter("nameVariety");
+        int newVarietyId = Integer.parseInt(request.getParameter("product"));
+        varietyService.update(id, newNameVariety, newVarietyId);
+
+        return "redirect:/variety";
+    }
+
+    @DeleteMapping("/id{id}")
+    public String delete(@PathVariable("id") int id) {
+        productService.delete(id);
 
         return "redirect:/variety";
     }
--- src/main/java/by/VIWARR/SpringJPA/services/VarietyService.java
+++ src/main/java/by/VIWARR/SpringJPA/services/VarietyService.java
@@ -4,10 +4,12 @@ import by.VIWARR.SpringJPA.models.Product;
 import by.VIWARR.SpringJPA.models.Variety;
 import by.VIWARR.SpringJPA.repositories.VarietyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,7 +28,7 @@ public class VarietyService {
 
     /*----------Отображение всех сортов-----------*/
     public List<Variety> findAll() {
-        return varietyRepository.findAll();
+        return varietyRepository.findAll(Sort.by("nameVariety"));
     }
 
 
@@ -36,5 +38,27 @@ public class VarietyService {
         Product product = productService.findOne(idProduct);
         Variety variety = new Variety(nameVariety, product);
         varietyRepository.save(variety);
+    }
+
+    /*---------Поиск сорта в Id----------*/
+    public Variety findOne(int id) {
+        Optional<Variety> variety = varietyRepository.findById(id);
+        return variety.orElse(null);
+    }
+
+    @Transactional
+    public void update(int id, String newVarietyName, int newVarietyId) {
+        Variety variety = findOne(id);
+        variety.setNameVariety(newVarietyName);
+
+        Product product = productService.findOne(newVarietyId);
+        variety.setProduct(product);
+
+        varietyRepository.save(variety);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        varietyRepository.deleteById(id);
     }
 }
